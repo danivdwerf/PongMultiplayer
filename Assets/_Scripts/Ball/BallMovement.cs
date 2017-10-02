c3affa1a938c27ad1bb48d08a0e252fe6a592185
danivdwerf@gmail.com <danivdwerf@gmail.com>
2017-10-02 16:39:18 +0200

    created ball logic

---
+++ Assets/_Scripts/Ball/BallMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+public class BallMovement : MonoBehaviour 
+{
+    [SerializeField]private float speedMultiplier = 1;
+
+    private Vector2 velocity;
+    private Rigidbody2D rigid;
+    private CircleCollider2D collider;
+
+    private void Start()
+    {
+        this.rigid = this.GetComponent<Rigidbody2D>();
+        this.rigid.gravityScale = 0;
+
+        this.collider = this.GetComponent<CircleCollider2D>();
+        this.collider.isTrigger = true;
+
+        this.velocity = new Vector2(1, 0.5f);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 test = this.velocity * this.speedMultiplier * Time.deltaTime;
+        rigid.MovePosition(this.rigid.position + test);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        this.velocity = Vector3.Reflect(-this.velocity, other.transform.forward);
+    }
+}
--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour 
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+public class PlayerMovement : NetworkBehaviour 
 {
     [SerializeField]private float speedMultiplier = 2.0f;
     private Rigidbody2D rigid;
+    private BoxCollider2D collider;
     private Vector2 movement;
 
-    private void Start()
+    public override void OnStartLocalPlayer()
     {
+        this.transform.position = new Vector2(-8, 0);
         this.rigid = this.GetComponent<Rigidbody2D>();
         this.rigid.gravityScale = 0;
+
+        this.collider = this.GetComponent<BoxCollider2D>();
 
         this.movement = new Vector2(0, 0);
     }
 
     private void Update()
     {
+        if (!isLocalPlayer)
+            return;
         movement = new Vector2(0, Input.GetAxis("Vertical"));
     }
 
     private void FixedUpdate()
     {
+        if (!isLocalPlayer)
+            return;
+        
         Vector2 Velocity = movement.normalized * speedMultiplier * Time.deltaTime;
         this.rigid.MovePosition(this.rigid.position + Velocity);
     }
